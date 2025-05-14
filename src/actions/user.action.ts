a8325e89b9eda987668fd4c316280f595ddb7625
Bader Zoabi <bader.zoabi@gmail.com>
2025-05-14 17:33:32 +0200

    Fix Prisma Generated Files And Sync User With The Navbar

---
+++ src/actions/user.action.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+// Take the user from Clerk and save it to the database
+// This function is called when the user signs in or signs up
+// It will create a new user in the database if it doesn't exist
+// If the user already exists, it will return the existing user
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) return;
+
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    if (existingUser) return existingUser;
+
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+
+    return dbUser;
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+}
